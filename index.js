b21d6a1c0d1df0f18518b5ad7db6d492e9e7eb47
Calypso Leonard <calypsorleonard@gmail.com>
2022-03-08 11:17:49 -0500

    refactoring, styling, making cards fit into guess grid cells

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ const winningDiv = document.querySelector("#winningDiv");
 const availableCardsDiv = document.getElementById("availableCardsDiv");
 
 let currentGuessArray = [];
-let deckID;
 let guessCounter = 0;
 let winning = false;
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
   appendGoalCards();
   setGoalCards();
-  
+  setAvailableCards();
+  createGuessGrid();
+});
+
+function setAvailableCards() {
   fetch("https://deckofcardsapi.com/api/deck/new/")
   .then((response) => response.json())
   .then((data) => {
-    deckID = data.deck_id;
-    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`)
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log(data.cards)
-      data.cards.forEach((element) => {
-        //console.log(element)
-        const tempCard = document.createElement("div");
-        tempCard.className = "guessableCards";
-        
-        const tempImg = document.createElement("img");
-        tempImg.src = element.images.png;
-        tempImg.addEventListener("click", (e) => {
-          if (currentGuessArray.length <= 4) {
-            if (!currentGuessArray.includes(element.code)) {
-              //console.log(currentGuessArray)
-              console.log(element);
+      const deckID = data.deck_id;
+      fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`)
+        .then((response) => response.json())
+        .then((data) => {
+          //console.log(data.cards)
+          data.cards.forEach((element) => {
+            //console.log(element)
+            const tempCard = document.createElement("div");
+            tempCard.className = "guessableCards";
+
+            const tempImg = document.createElement("img");
+            tempImg.src = element.images.png;
+            tempImg.addEventListener("click", (e) => {
+              if (currentGuessArray.length <= 4) {
+                if (!currentGuessArray.includes(element.code)) {
+                  //console.log(currentGuessArray)
+                  console.log(element);
                   addGuess(e);
                 }
               }
@@ -45,8 +46,8 @@ document.addEventListener("DOMContentLoaded", () => {
             availableCardsDiv.appendChild(tempCard);
           });
         });
-      });
-});
+    });
+}
 
 function setGoalCards() {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
@@ -58,7 +59,6 @@ function setGoalCards() {
       fetch(`https://deckofcardsapi.com/api/deck/${goalDeckID}/draw/?count=5`)
         .then((response) => response.json())
         .then((data) => {
-          
           data.cards.forEach((card) => {
             console.log(card.code); //leave in for testing
             const cardFace = document.createElement("img");
@@ -76,8 +76,8 @@ function appendGoalCards() {
     const cardImg = document.createElement("img");
     cardImg.classList.add("cardBack");
     let thisGoal = document.querySelector(`#goal${i}`);
-    
-    if (!winning){
+
+    if (!winning) {
       cardImg.src = "assets/card.png";
       thisGoal.append(cardImg);
     } else {
@@ -87,21 +87,36 @@ function appendGoalCards() {
   }
 }
 
-
 function createGuessGrid() {
   // is it better to add grid dynamically?
+  const gridDiv = document.getElementById("allGuessedCardsDiv")
+
+  for (let i = 0; i < 5; i++){
+    const row = document.createElement('div')
+    row.classList.add("guessed")
+    row.id = `guessedCards${i}`
+    for (let j = 0; j < 5; j++){
+      const cell = document.createElement('div')
+      cell.classList.add("guessBox", `guess${j}`)
+      //cell.id = `guess${j}`
+      row.appendChild(cell)
+    }
+    gridDiv.append(row)
+  }
+
 }
 
-
 function addGuess(e) {
-  const guessedCardDiv = document.createElement("div");
+  const guessedCardRow = document.querySelector(`#guessedCards${guessCounter}`)
+  //console.log(guessedCardRow)
+  const guessedCardCell = guessedCardRow.querySelector(`.guess${currentGuessArray.length}`);
 
   const guessedCardImage = document.createElement("img");
   guessedCardImage.src = e.target.src;
 
   const currentID = e.target.id;
   currentGuessArray.push(currentID);
-  guessedCardDiv.classList.add(`guess${currentID}`, `guessedCard`);
+  guessedCardCell.classList.add(`guess${currentID}`, `guessedCard`);
 
   guessedCardImage.addEventListener("click", (e) => {
     const indexToRemove = currentGuessArray.indexOf(currentID);
@@ -109,18 +124,18 @@ function addGuess(e) {
     e.target.parentNode.remove();
   });
 
-  const guessedCardSpace = document.getElementById(
-    `guessedCards${guessCounter}`
-  );
+  // const guess = document.getElementById(
+  //   `guessedCards${guessCounter}`
+  // );
 
-  guessedCardDiv.append(guessedCardImage);
-  guessedCardSpace.appendChild(guessedCardDiv);
+  guessedCardCell.append(guessedCardImage);
+  //guessedCardSpace.appendChild(guessedCardDiv);
 
   //if top id == bottom id (ie AD, 2D), then toggle mouseclick/pointer event
 }
 
 function handleSubmit(e) {
-  const currentGuessDiv = document.querySelector(
+  const currentGuessRow = document.querySelector(
     `#guessedCards${guessCounter}`
   );
   const commonCards = goalCardsArray.filter((id) => {
@@ -134,7 +149,7 @@ function handleSubmit(e) {
   } else {
     currentGuessArray.forEach((guess) => {
       const resultText = document.createElement("p");
-      const thisGuessDiv = currentGuessDiv.querySelector(`.guess${guess}`);
+      const currentGuessCell = currentGuessRow.querySelector(`.guess${guess}`);
       if (goalCardsArray.includes(guess)) {
         resultText.textContent = "IN";
         resultText.classList.add("in");
@@ -142,8 +157,8 @@ function handleSubmit(e) {
         resultText.textContent = "NOT IN";
         resultText.classList.add("notIn");
       }
-      thisGuessDiv.appendChild(resultText);
-      thisGuessDiv.classList.add("blocked");
+      currentGuessCell.appendChild(resultText);
+      currentGuessCell.classList.add("blocked", "submitted");
     });
   }
   currentGuessArray = [];
@@ -153,19 +168,19 @@ function handleSubmit(e) {
     submitBtn.classList.add("blocked");
   }
 }
-  //put the buttons in the init function? or just they should live at top
-  
-  const submitBtn = document.querySelector("#submitBtn");
-  submitBtn.addEventListener("click", (e) => {
-    // alert if guess array length is < 5 don't take more guesses
-    if (currentGuessArray.length < 5) {
-      alert("You need 5 cards to guess");
-    } else {
-      handleSubmit(e);
-    }
-  });
-  
-  const resetBtn = document.getElementById("resetBtn");
-  resetBtn.addEventListener("click", () => {
-    document.location.reload();
-  });
+//put the buttons in the init function? or just they should live at top
+
+const submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", (e) => {
+  // alert if guess array length is < 5 don't take more guesses
+  if (currentGuessArray.length < 5) {
+    alert("You need 5 cards to guess");
+  } else {
+    handleSubmit(e);
+  }
+});
+
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", () => {
+  document.location.reload();
+});
