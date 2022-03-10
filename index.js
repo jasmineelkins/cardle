170a9e76c8e8295f484f1fa18b0b8b04f254f140
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-03-10 15:51:59 -0500

    Refactored handleSubmit() and getHint(), started consolidating button classes

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ let rowGuessCounter = 0;
 let gameEnd = false;
 let losing = true;
 let contrastMode = false;
+let hintClicked = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  appendGoalCards();
   setGoalCards();
+  appendGoalCards();
+  // createHintArray();
   setAvailableCards();
   createGuessGrid();
   populateButtons();
@@ -76,6 +78,7 @@ const renderCardFace = async (goalDeckID) => {
     goalCardsArray.push(card.code);
     goalCardImagesArray.push(card.image);
   });
+  createHintArray();
 };
 
 const setGoalCards = async () => {
@@ -159,125 +162,103 @@ function addGuess(e) {
   rowGuessCounter++;
 }
 
+const handleResultStyling = (
+  suitDiv,
+  currentGuessSuit,
+  valueDiv,
+  currentGuessValue
+) => {
+  let suitIcon;
+  currentGuessValue === "0"
+    ? (valueDiv.textContent = "10")
+    : (valueDiv.textContent = currentGuessValue);
+
+  switch (currentGuessSuit) {
+    case "H":
+      suitIcon = "❤️";
+      break;
+    case "S":
+      suitIcon = "♠️";
+      break;
+    case "C":
+      suitIcon = "♣️";
+      break;
+    case "D":
+      suitIcon = "♦️";
+      break;
+  }
+  suitDiv.textContent = suitIcon;
+};
+
+const testMatch = (testCase, checkIndex, currentGuessCase) => {
+  let i = 0;
+  while (!testCase && i < 5) {
+    if (currentGuessCase === goalCardsArray[i][checkIndex]) {
+      testCase = true;
+    }
+    i++;
+  }
+  return testCase;
+};
+
 function handleSubmit() {
   const currentGuessRow = document.querySelector(
     `#guessedCards${guessCounter}`
   );
 
-  const commonCards = goalCardsArray.filter((id) => {
-    return currentGuessArray.includes(id);
-  });
-
   for (let i = 0; i < 5; i++) {
     const currentGuessCell = currentGuessRow.querySelector(`.guess${i}`);
+    const resultDiv = createDivElement(["results"]);
+    const valueDiv = createDivElement(["value"]);
+    const suitDiv = createDivElement(["suit"]);
+    const currentGuessValue = currentGuessArray[i][0];
+    const currentGuessSuit = currentGuessArray[i][1];
+    const matchInGuessable = document.getElementById(currentGuessArray[i]);
 
-    const resultDiv = document.createElement("div");
-    const valueDiv = document.createElement("div");
-    const suitDiv = document.createElement("div");
     let suitMatch = false;
     let valueMatch = false;
     let allMatch = false;
 
-    const currentGuessValue = currentGuessArray[i][0];
-    const currentGuessSuit = currentGuessArray[i][1];
-    const matchInGuessable = document.getElementById(currentGuessArray[i]);
-    let suitIcon;
+    // CAN BE REMOVED ***
+    if (contrastMode) {
+      resultDiv.classList.add("contrast");
+      valueDiv.classList.add("contrast");
+      suitDiv.classList.add("contrast");
+    }
 
     currentGuessCell.classList.add("blocked", "submitted");
-    resultDiv.classList.add("results");
-    suitDiv.classList.add("suit");
-    valueDiv.classList.add("value");
+    handleResultStyling(suitDiv, currentGuessSuit, valueDiv, currentGuessValue);
 
-    if (currentGuessValue === "0") {
-      valueDiv.textContent = "10";
-    } else {
-      valueDiv.textContent = currentGuessValue;
-    }
-
-    switch (currentGuessSuit) {
-      case "H":
-        suitIcon = "❤️";
-        break;
-      case "S":
-        suitIcon = "♠️";
-        break;
-      case "C":
-        suitIcon = "♣️";
-        break;
-      case "D":
-        suitIcon = "♦️";
-        break;
-    }
-    suitDiv.textContent = suitIcon;
-
+    // determines if currentGuess fully matches anything in goalCards
     if (goalCardsArray.includes(currentGuessArray[i])) {
       allMatch = true;
-      suitMatch = true;
-      valueMatch = true;
       resultDiv.textContent = "QARD!";
-
       matchInGuessable.classList.add("qard");
       resultDiv.classList.add("allMatch");
-
-      if (!contrastMode) {
-        resultDiv.classList.add("allMatch");
-      } else {
-        resultDiv.classList.add("allMatch", "contrast");
-      }
     } else {
+      // determine if currentGuess is partial match
       matchInGuessable.classList.add("eliminated");
-      let i = 0;
-
-      while (!valueMatch && i < 5) {
-        if (currentGuessValue === goalCardsArray[i][0]) {
-          valueMatch = true;
-        }
-        i++;
-      }
-
-      let j = 0;
-      while (!suitMatch && j < 5) {
-        if (currentGuessSuit === goalCardsArray[j][1]) {
-          suitMatch = true;
-        }
-        j++;
-      }
+      valueMatch = testMatch(valueMatch, 0, currentGuessValue);
+      suitMatch = testMatch(suitMatch, 1, currentGuessSuit);
     }
 
-    if (valueMatch) {
-      if (!contrastMode) {
-        valueDiv.classList.add("correct");
-      } else {
-        valueDiv.classList.add("correct", "contrast");
-      }
-    } else {
-      if (!contrastMode) {
-        valueDiv.classList.add("wrong");
-      } else {
-        valueDiv.classList.add("wrong", "contrast");
-      }
-    }
+    let testResult;
+    valueMatch ? (testResult = "correct") : (testResult = "wrong");
+    valueDiv.classList.add(testResult);
 
-    if (suitMatch) {
-      if (!contrastMode) {
-        suitDiv.classList.add("correct");
-      } else {
-        suitDiv.classList.add("correct", "contrast");
-      }
-    } else {
-      if (!contrastMode) {
-        suitDiv.classList.add("wrong");
-      } else {
-        suitDiv.classList.add("wrong", "contrast");
-      }
-    }
+    suitMatch ? (testResult = "correct") : (testResult = "wrong");
+    suitDiv.classList.add(testResult);
 
     if (!allMatch) {
       resultDiv.append(valueDiv, suitDiv);
     }
-
     currentGuessCell.append(resultDiv);
   }
+
+  const commonCards = goalCardsArray.filter((id) => {
+    return currentGuessArray.includes(id);
+  });
+
   if (commonCards.length === 5) {
     losing = false;
     endGame();
@@ -292,7 +273,9 @@ function handleSubmit() {
   rowGuessCounter = 0;
 }
 
-function makeHint() {
+let hintArray = [];
+
+function createHintArray() {
   let spadeString = " spades";
   let clubString = " clubs";
   let diamondString = " diamonds";
@@ -333,98 +316,120 @@ function makeHint() {
     heartString = " heart";
   }
 
-  //console.log("spades", spadeNum, "clubs ", clubNum, " diamonds ", diamondNum, " hearts ", heartNum)
-
-  const hintArray = [
+  hintArray.push(
     spadeNum + spadeString,
     clubNum + clubString,
     diamondNum + diamondString,
-    heartNum + heartString,
-  ];
-  const randomIndex = Math.floor(Math.random() * hintArray.length);
-  const hint = `This QARDLE has ${hintArray[randomIndex]}.`;
+    heartNum + heartString
+  );
+
+  return hintArray;
+}
+
+function getHint() {
+  const remainingHints = hintArray.length;
+  const randomIndex = Math.floor(Math.random() * remainingHints);
+  let remainingHintsText = ` You have ${remainingHints - 1} hints remaining.`;
+  if (remainingHints === 2) {
+    remainingHintsText = ` You have 1 hint remaining.`;
+  } else if (remainingHints === 1) {
+    remainingHintsText = `This is your last hint, good luck!`;
+  }
+  let hint = `This QARDLE has ${hintArray[randomIndex]}.` + remainingHintsText;
+  if (remainingHints === 0) {
+    hint = "You have no more hints, sorry, not sorry.";
+  }
+  hintArray.splice(randomIndex, 1);
   return hint;
 }
 
 // Game End Function
 function endGame() {
-  submitBtn.classList.add("hidden");
-  giveUpBtn.classList.add("hidden");
-  hintBtn.classList.add("hidden");
-  availableCardsDiv.classList.add("hidden");
+  [submitBtn, giveUpBtn, hintBtn, availableCardsDiv].forEach((element) =>
+    element.classList.add("hidden")
+  );
   gameEnd = true;
   appendGoalCards();
   window.scrollTo(0, 0);
   Splitting();
 
-  if (!losing) {
-    const winningDiv = document.querySelector("#winningDiv");
-    winningDiv.classList.remove("hidden");
-  } else {
-    const losingDiv = document.querySelector("#losingDiv");
-    losingDiv.classList.remove("hidden");
-  }
+  const winningDiv = document.querySelector("#winningDiv");
+  const losingDiv = document.querySelector("#losingDiv");
+
+  losing
+    ? losingDiv.classList.remove("hidden")
+    : winningDiv.classList.remove("hidden");
 }
-const body = document.getElementsByTagName("body");
 
 // Buttons & Event Listeners
 function populateButtons() {
   const faqBtn = document.querySelector("#faqBtn");
+  const submitBtn = document.querySelector("#submitBtn");
+  const resetBtn = document.getElementById("resetBtn");
+  const giveUpBtn = document.querySelector("#giveUpBtn");
+  const hintBtn = document.getElementById("hintBtn");
+  const contrastBtn = document.querySelector("#contrastBtn");
+
   faqBtn.addEventListener("click", () => {
     const faqText = document.querySelector("#faqText");
     faqText.classList.toggle("hidden");
   });
 
   contrastBtn.addEventListener("click", () => {
-    const contrastBtn = document.querySelector("#contrastBtn");
     const contrastIcon = document.querySelector("#contrastIcon");
     const faqIcon = document.querySelector("#faqIcon");
     const content = document.querySelector("#content");
-    // const allButtons = document.getElementsByTagName("button");
 
-    contrastBtn.classList.toggle("selected");
-    contrastIcon.classList.toggle("selected");
     content.classList.toggle("contrast");
-    faqBtn.classList.toggle("selected");
-    faqIcon.classList.toggle("selected");
-    submitBtn.classList.toggle("selected");
-    resetBtn.classList.toggle("selected");
-    // allButtons.classList.add("selected");
 
-    if (!contrastMode) {
-      contrastMode = true;
-    } else {
-      contrastMode = false;
-    }
+    // FIX THIS ***
+    [
+      contrastBtn,
+      contrastIcon,
+      faqBtn,
+      faqIcon,
+      submitBtn,
+      resetBtn,
+      hintBtn,
+      giveUpBtn,
+      content,
+    ].forEach((element) => element.classList.toggle("selected"));
+
+    contrastMode ? !contrastMode : contrastMode;
   });
 
-  const submitBtn = document.querySelector("#submitBtn");
   submitBtn.addEventListener("click", () => {
     window.scrollTo(0, 350);
 
     if (currentGuessArray.includes("none")) {
-      //if any empty strings in array
       alert("You need 5 cards to guess");
     } else {
       handleSubmit();
     }
   });
 
-  const resetBtn = document.getElementById("resetBtn");
   resetBtn.addEventListener("click", () => {
     document.location.reload();
   });
 
-  const giveUpBtn = document.querySelector("#giveUpBtn");
   giveUpBtn.addEventListener("click", () => {
     losing = true;
     endGame();
   });
 
-  const hintBtn = document.getElementById("hintBtn");
   hintBtn.addEventListener("click", () => {
     const hintText = document.querySelector("#hintDiv");
-    hintText.textContent = makeHint();
+
     hintText.classList.toggle("hidden");
+
+    if (!hintClicked) {
+      hintText.textContent = getHint();
+      hintClicked = true;
+    } else {
+      hintClicked = false;
+      console.log(hintClicked);
+    }
   });
 }
+
+// select all buttons in a list ... buttonList.forEach(pass event listener) ??
